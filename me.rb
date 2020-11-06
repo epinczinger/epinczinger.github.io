2b3dd5f3892b37b25630b01ac38c8c38c8edd2c6
EstebanP <epinczinger@gmail.com>
2020-11-06 12:08:19 -0300

    add linters.yml, gradient color style

--- me.rb
+++ me.rb
@@ -1,36 +1,15 @@
-class Junior < SoftwareDeveloper
-    def initialize
-        @name = 'Esteban Pinczinger'
-        @skills = {
-            backend => ['Ruby']
-            frontend => ['HTML5', 'CSS3', 'JavaScript', 'SASS']
-            frameworks => ['Rails', 'Bootstrap']
-            others => ['Spanish', 'Git', 'Maths']
-        }
-        @linkedin = 'Esteban Pinczinger'
-        @github = 'epinczinger'
-        @email = 'epinczinger@gmail.com'
-    end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# frozen_string_literal: true
+# class Junior < SoftwareDeveloper
+#     def initialize
+#         @name = 'Esteban Pinczinger'
+#         @skills = {
+#             backend => ['Ruby']
+#             frontend => ['HTML5', 'CSS3', 'JavaScript', 'SASS']
+#             frameworks => ['Rails', 'Bootstrap']
+#             others => ['Spanish', 'Git', 'Maths']
+#         }
+#         @linkedin = 'Esteban Pinczinger'
+#         @github = 'epinczinger'
+#         @email = 'epinczinger@gmail.com'
+#     end
+# end
